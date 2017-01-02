56f2de683c5cecc11dbb979a5a5eaa0ad4478ba9
SoulsideJourney <lalkolettov@gmail.com>
2017-01-02 15:10:36 +0500

    Класс Змейка

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,20 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+            
+            Point p = new Point(4, 5, '*');
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '#');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '#');
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '#');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+            
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Draw();
-
-            VerticalLine line2 = new VerticalLine(4, 3, 20, '/');
-            line2.Draw(); 
-
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
+
         }
     }
 }
