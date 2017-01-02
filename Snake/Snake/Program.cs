fe94a1540d28b7463a40b90d172fed17973f859d
SoulsideJourney <lalkolettov@gmail.com>
2017-01-02 16:11:38 +0500

    Еда

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,8 +13,6 @@ namespace Snake
         {
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
-            
-            Point p = new Point(4, 5, '*');
 
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '#');
@@ -25,17 +23,29 @@ namespace Snake
             leftLine.Draw();
             rightLine.Draw();
 
+            Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
 
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else snake.Move();
+                Thread.Sleep(100);            
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.Handlekey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
            
         }
