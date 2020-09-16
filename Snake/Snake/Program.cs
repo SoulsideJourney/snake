e670d6bf1410a2b61fcba787749faa926c9da3cd
Alpha Ori <souljrn@gmail.com>
2020-09-16 18:07:56 +0500

    Добавлено сообщение о СМЭРТи, вынесена функция StartTime

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,44 +9,60 @@ namespace Snake
 {
     class Program
     {
+        static Walls walls;
+        static Snake snake;
+        static Point food;
+        static FoodCreator foodCreator;
+
         static void Main(string[] args)
         {
             //В начале было Слово, и Слово это -- Бох
 
             //И вот создал Бох Вселенную, Эа Сущую
+            Console.CursorVisible = false;
+            //int h = Console.BufferHeight;
+            // w = Console.BufferWidth;
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
 
             //Потом создал он великие Стены
-            Walls walls = new Walls(80, 25);
+            walls = new Walls(80, 25);
             walls.Draw();
 
             //Потом первую живую тварь
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
             //Потом первую еду, ибо хотела тварь вкушать пищу
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
+            foodCreator = new FoodCreator(80, 25, '$');
+            food = foodCreator.CreateFood();
             food.Draw();
 
-            //Потом создал Бох время 
+            StartTime();                    
+        }
+
+        static void StartTime() //Потом создал Бох время 
+        {
             while (true)
             {
                 if (walls.IsHit(snake) || snake.IsHitTail())
                 {
+                    Console.SetCursorPosition(36, 12);
+                    Console.WriteLine("СМЭРТЬ");
+                    Console.ReadKey();
                     break;
                 }
+
                 if (snake.Eat(food))
                 {
-                    Console.BackgroundColor = ConsoleColor.Cyan;    //Цвет еды
+                    Console.ForegroundColor = ConsoleColor.Cyan;    //Цвет еды
                     food = foodCreator.CreateFood();
                     food.Draw();
-                    Console.BackgroundColor = ConsoleColor.White;   //Цвет всего остального
+                    Console.ForegroundColor = ConsoleColor.White;   //Цвет всего остального
                 }
                 else snake.Move(); //И вот приказал Он своей твари бесконечно бегать среди Великих стен, ибо хз че ещё делать
-                Thread.Sleep(100);            
+                Thread.Sleep(100);
 
                 if (Console.KeyAvailable)
                 {
@@ -54,7 +70,6 @@ namespace Snake
                     snake.Handlekey(key.Key);
                 }
             }
-           
         }
     }
 }
